446968973ebba06ff9756a685b49d3dbd1df3cb0
Robin <robin.soenen@gmail.com>
2018-03-03 13:06:29 +0100

    Add unit tests

--- tests/diacritics.test.js
+++ tests/diacritics.test.js
@@ -1,5 +1,33 @@
 const cleaner = require('../diacritics');
 
-test('cleans crême brûléè to march creme brulee', () => {
+test("Cleans aAáÁâÂàÀäÂ  to aAaAaAaAaA", () => {
+  expect(cleaner.clean("aAáÁâÂàÀäÂ")).toBe('aAaAaAaAaA');
+});
+
+test("Cleans eEéÉêÊèÈëË  to eEeEeEeEeE", () => {
+  expect(cleaner.clean("eEéÉêÊèÈëÊ")).toBe('eEeEeEeEeE');
+});
+
+test("Cleans oOóÓôÔòÒöÖ  to oOoOoOoO", () => {
+  expect(cleaner.clean("oOóÓôÔòÒöÖ")).toBe('oOoOoOoOoO');
+});
+
+test("Cleans iIíÍîÎìÌïÏ  to iIiIiIiI", () => {
+  expect(cleaner.clean("iIíÍîÎìÌïÏ")).toBe('iIiIiIiIiI');
+});
+
+test("Cleans uUúÚûÛùÙüÜ  to uUuUuUuU", () => {
+  expect(cleaner.clean("uUúÚûÛùÙüÜ")).toBe('uUuUuUuUuU');
+});
+
+test("cleans vicuña to vicuna", () => {
+  expect(cleaner.clean('vicuña')).toBe('vicuna');
+});
+
+test("cleans Việt Nam to Viet Name", () => {
+  expect(cleaner.clean('Việt Nam')).toBe('Viet Nam');
+});
+
+test('Cleans crême brûléè to match creme brulee', () => {
   expect(cleaner.clean('crême brûléè')).toBe('creme brulee');
 });
